630e66db384198dc08cd022f824e089f14b6c479
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2019-04-28 18:46:11 -0500

    Updates

---
+++ precache-manifest.894a62279657dbf9b46dbe5f7277e1e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0d1a2ca991350a555ba4754700f820",
+    "url": "/animaal/index.html"
+  },
+  {
+    "revision": "8f004f560bf9b611c808",
+    "url": "/animaal/static/css/main.c4e69109.chunk.css"
+  },
+  {
+    "revision": "680cf0913746a9b4cbfe",
+    "url": "/animaal/static/js/2.ce880dd3.chunk.js"
+  },
+  {
+    "revision": "8f004f560bf9b611c808",
+    "url": "/animaal/static/js/main.fca89ab9.chunk.js"
+  },
+  {
+    "revision": "74c74b919cb4b5e87632",
+    "url": "/animaal/static/js/runtime~main.4a47245c.js"
+  }
+]);
