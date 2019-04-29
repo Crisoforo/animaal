2f42d058ba047d769c489e2fffbb5e341562bb57
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2019-04-28 21:05:28 -0500

    bellychain

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import aaa from './images/aaa.png';
 function App() {
   return (
     <div>
-      <Navbar brand={ <a className="brand-logo"> <img src={ logo } alt='logo' width='150'/></a>} centerLogo alignLinks="right">
+      <Navbar brand={ <a href='index.html' className="brand-logo"> <img src={ logo } alt='logo' width='150'/></a>} centerLogo alignLinks="right">
         <NavItem href="https://www.instagram.com/animaal.tv/?hl=es">
           Instagram
 </NavItem>
@@ -19,27 +19,27 @@ function App() {
       </Navbar>
     <div className="card">
       <h3>Agencia consultora para el desarrollo musical</h3>
-      <img src={aaa}/>
+      <img src={aaa} alt='animaal logo'/>
     </div>
     <div className="card">
       <h3>MANAGEMENT BOOKING/ TOURING TALENT BUYERS</h3>
-      <img src={aaa}/>
+      <img src={aaa} alt='animaal logo'/>
     </div>
     <div className="card">
       <h3>Administración y representación del talento.</h3>
-      <img src={aaa}/>
+      <img src={aaa} alt='animaal logo'/>
     </div>
     <div className="card">
       <h3>Venta y comercialización del talento.</h3>
-      <img src={aaa}/>
+      <img src={aaa} alt='animaal logo'/>
     </div>
     <div className="card">
       <h3>Venta y comercialización del talento fuera de su ciudad.</h3>
-      <img src={aaa}/>
+      <img src={aaa} alt='animaal logo'/>
     </div>
     <div className="cardee">
       <h3>Nos encargamos de comprar y programar artistas y/o conciertos para un foro, marca o festival.</h3>
-      <img src={aaa}/>
+      <img src={aaa} alt='animaal logo'/>
     </div>
     <div className="cardee">
     <h2>Roster</h2>
@@ -55,11 +55,11 @@ function App() {
         <li>OM BEACH (MX)</li>
         <li>VIDEOCENTRO (MX)</li>
       </ul>
-      <img src={aaa}/>
+      <img src={aaa} alt='animaal logo'/>
     </div>
     <div className="card" id='contact'>
     <div className='brnd'>
-    <img src={mgnt} width='90%'/>
+    <img src={mgnt} width='90%' alt='management'/>
     </div>
       <h3>Gerardo A. Lopez Avila</h3>
       <p>+52 1 (55) 55 09 49 36<br></br>Amsterdam 255,<br></br>Colonia Condesa, <br></br>CDMX</p>
